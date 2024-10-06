e636b4080bc44c968c2419eac7e076908f12304f
bindu <bindu.tata@gmail.com>
2024-10-06 08:50:48 +0530

    popup windows  completed

---
+++ src/components/PopUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const SearchBar=()=>{
+    return(
+        <>
+            <input className='form-control' type='search' placeholder='search'/>
+            <button className='btn btn-light'>search</button>
+        </>
+    )
+}
+
