73cb8a325fcaa5fd2b54e3ba95ace86dd9cec2a5
bindu <bindu.tata@gmail.com>
2024-10-06 08:49:34 +0530

    advantages  completed

---
+++ src/components/Advantages/Advantages.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {advantagesData} from './data';
+import '../Advantages/styles.css'
+
+const Advantages=()=>{
+   return(
+        <div className="container-fluid">
+            <h2>OUR ADVANTAGES</h2>
+            <div className="services">
+                {advantagesData.map((item)=>(
+                    <div className="advantages">
+                        <div className="icon">
+                            {item.icon}
+                        </div>
+                        <div className="desc">
+                            <p className="title">{item.title}</p>
+                            <p className="info">{item.info}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            
+            
+
+        </div>
+   )
+};
+export default Advantages;
+
---
+++ src/components/Advantages/data.js
@@ -0,0 +1,28 @@
+import {FaShippingFast, FaShieldAlt, FaTags, FaCreditCard} from 'react-icons/fa';
+
+export const advantagesData=[
+    {
+        id: 1,
+        icon: <FaShippingFast />,
+        title: "Express Delivery",
+        info: "Ships in 24 Hours",
+    },
+    {
+        id: 2,
+        icon: <FaShieldAlt />,
+        title: "Brand Warranty",
+        info: "100% Original products",
+    },
+    {
+        id: 3,
+        icon: <FaTags />,
+        title: "Exciting Deals",
+        info: "On all prepaid orders",
+    },
+    {
+        id: 4,
+        icon: <FaCreditCard />,
+        title: "Secure Payments",
+        info: "SSL / Secure сertificate",
+    },
+]
